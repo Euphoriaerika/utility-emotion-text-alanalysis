46dd1049d48372f0f25771b8944010dafaa16007
Bohdan Rudenko <b.r.rudenko@student.khai.edu>
2024-09-21 02:52:49 +0300

    delete lang detector

--- src/emodji.py
+++ src/emodji.py
@@ -1,15 +1,13 @@
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
-from translator import detect_and_translate
+from translator import translate
 
 
 def emotionAnalysis(text):
     # Translate the text to English if it's not already
-    text = detect_and_translate(text)
+    text = translate(text)
 
     # Create a sentiment analyzer
     analyzer = SentimentIntensityAnalyzer()
-
     # Scores are between -1 and 1 for negative, neutral and positive
     scores = analyzer.polarity_scores(text)
-
     return scores
--- src/translator.py
+++ src/translator.py
@@ -1,16 +1,10 @@
-from langdetect import detect
 from googletrans import Translator
 
 
-def detect_and_translate(text):
-    language = detect(text)
+def translate(text):
+    # Create a translator
+    translator = Translator()
+    # Translate the text to English
+    translated_text = translator.translate(text, dest="en").text
 
-    if language != "en":
-        # Create a translator
-        translator = Translator()
-        # Translate the text to English
-        translated_text = translator.translate(text, dest="en").text
-        return translated_text
-
-    # If the text is already in English, return it as it is
-    return text
+    return translated_text
