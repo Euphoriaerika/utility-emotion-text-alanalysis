0c61ec69043f7b35fd256cd5385af2fb892abd0d
Bohdan Rudenko <b.r.rudenko@student.khai.edu>
2024-09-21 01:55:15 +0300

    added emotion recognition using varder

--- src/emodji.py
+++ src/emodji.py
@@ -1,8 +1,16 @@
+from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from translator import detect_and_translate
+
 def emotionAnalysis(text):
+    text = detect_and_translate(text)
+
     # Simple logic for emotion analysis
-    if "!" in text or "дуже подобається" in text:
-        return "Позитивне"
-    elif "?" in text:
-        return "Питальне"
-    else:
-        return "Нейтральне"
+    analyzer = SentimentIntensityAnalyzer()
+
+    # Scores are between -1 and 1 for negative, neutral and positive
+    scores = analyzer.polarity_scores(text)
+
+    return scores
+
+print(emotionAnalysis("I love programming in Python!"))
+print(emotionAnalysis("Я люблю програмування на Python!"))
